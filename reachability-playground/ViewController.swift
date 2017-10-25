428208ed0a36b0245d2830c143f7b069835421be
Neo Ighodaro <neo@creativitykills.co>
2017-10-25 14:05:44 +0100

    Initial Commit

---
+++ reachability-playground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reachability-playground
+//
+//  Created by Neo Ighodaro on 25/10/2017.
+//  Copyright © 2017 CreativityKills Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
